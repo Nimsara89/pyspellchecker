b0d94e0bd243885bfd46a134dc927838a1952183
Osanda Senarathne <osandanimsarasenarathne@gmail.com>
2024-10-14 21:14:56 +0530

    comremove
    remove comments

--- Check01.py
+++ Check01.py
@@ -1,9 +1,5 @@
 from spellchecker import SpellChecker
-
-# Initialize the spell checker
 spell = SpellChecker()
-
-# Input paragraph from the user
 paragraph = input("Please enter a paragraph: ")
 
 # Tokenize the paragraph into words
@@ -12,7 +8,7 @@ words = paragraph.split()
 # Find misspelled words
 misspelled = spell.unknown(words)
 
-# Correct the spelling mistakes
+# Correcting
 corrected_paragraph = []
 for word in words:
     if word in misspelled:
@@ -21,8 +17,6 @@ for word in words:
         corrected_paragraph.append(corrected_word)
     else:
         corrected_paragraph.append(word)
-
-# Join the words back into a corrected paragraph
 corrected_text = ' '.join(corrected_paragraph)
 
 # Output the original and corrected paragraphs
